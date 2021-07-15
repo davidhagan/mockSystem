6cc92a43dce5b89b02707235bc1b49a0c943a0f7
David Hagan <dave@ucroo.com>
2021-07-15 14:10:16 +1000

    added a test class

---
+++ src/test/java/test.java
@@ -0,0 +1,12 @@
+package mockSystem
+
+class TestPhoneNumbers {
+	public static void TestMobilePhoneNumber(){
+	}
+	public static void TestLandlinePhoneNumber(){
+	}
+	public static void TestInternationalPhoneNumber(){
+	}
+	public static void TestInvalidPhoneNumber(){
+	}
+}
