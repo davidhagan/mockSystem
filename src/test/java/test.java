914dbacaab141d4d1f2b9d7e3371bff8c4218adf
David Hagan <dave@ucroo.com>
2021-07-15 14:15:39 +1000

    added a gitignore, and some test data

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,11 +2,15 @@ package mockSystem
 
 class TestPhoneNumbers {
 	public static void TestMobilePhoneNumber(){
+		string input = "0423 544 575";
 	}
 	public static void TestLandlinePhoneNumber(){
+		string input = "03 9553 5534";
 	}
 	public static void TestInternationalPhoneNumber(){
+		string input = "+61 3 5955 2332";
 	}
 	public static void TestInvalidPhoneNumber(){
+		string input = "345 3453";
 	}
 }
